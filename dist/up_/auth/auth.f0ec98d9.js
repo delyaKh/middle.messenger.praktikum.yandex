d24b989b7b71e1c431cd048fcfd0f1c349257bc8
DelyaK1 <adelya.khabibullina@mail.ru>
2022-12-06 23:29:02 +0300

    fix command

---
+++ dist/up_/auth/auth.f0ec98d9.js
@@ -0,0 +1,2 @@
+window.btnHandler=function(){const e=""==document.querySelector("#login").value,o=""==document.querySelector("#password").value;e||o||(document.querySelector("#login").value="",document.querySelector("#password").value="",window.location.href="/up_/chat/chat.html")};
+//# sourceMappingURL=auth.f0ec98d9.js.map
